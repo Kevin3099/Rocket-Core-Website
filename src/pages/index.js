cfbab4e39d6ec152122232c6c393fe7a55eb9d24
Ben Silverman <bensilverman10@gmail.com>
2020-08-16 10:54:27 +0100

    Import styles and Layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,21 @@
-import React from "react"
+import React from "react";
 
-export default function Home() {
-  return <div>Hello world!2</div>
-}
+// Custom Components
+import Layout from "../components/layout";
+
+// Custom styles
+import "../styles/pages/index.scss";
+
+const Index = () => (
+	/* We can now use the custom "Layout" component like a normal HTML element.
+	   title="Home" is an example of a "prop" (property), which is data we can pass
+	   to the component for it to use when its rendered. In this case, the Layout
+	   component can use the "title" prop to dynamically adjust the <head> of the page
+	   whenever we change pages.
+	*/
+	<Layout title="Home">
+		<h1>Hello, world!</h1>
+	</Layout>
+);
+
+export default Index;
