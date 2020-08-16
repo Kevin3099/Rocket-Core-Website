37933caad76824f16c33fd115d7d47396b752e73
Ben Silverman <bensilverman10@gmail.com>
2020-08-16 10:50:22 +0100

    Add temp navbar and footer components

---
+++ src/components/footer.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Footer = () => <p>This is where the footer would go</p>;
+
+export default Footer;
---
+++ src/components/navbar.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Navbar = () => <p>This is where the navbar would go</p>;
+
+export default Navbar;
